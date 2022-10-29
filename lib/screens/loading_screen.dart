ae2f3781c2508a2387f8200b23fc64525b98d5e8
bruno-menslin <brunomenslin@outlook.com>
2022-10-29 19:08:34 -0300

    move 'checkLocationPermission' to 'Location' class

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -10,30 +10,6 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  Future<void> checkLocationPermission() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      // serviço de localização desabilitado. Não será possível continuar
-      return Future.error('O serviço de localização está desabilitado.');
-    }
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        // Sem permissão para acessar a localização
-        return Future.error('Sem permissão para acesso à localização');
-      }
-    }
-    if (permission == LocationPermission.deniedForever) {
-      // permissões negadas para sempre
-      return Future.error(
-          'A permissão para acesso a localização foi negada para sempre. Não é possível pedir permissão.');
-    }
-  }
-
   Future<void> getLocation() async {
     {
       await checkLocationPermission(); //veridicar permissao de acesso
