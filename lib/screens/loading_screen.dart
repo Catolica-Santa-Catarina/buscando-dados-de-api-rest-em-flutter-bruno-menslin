d15882aa1daade3ec10c8a049b63d6690a600bda
bruno-menslin <brunomenslin@outlook.com>
2022-10-29 19:24:48 -0300

    remove geolocator package from 'loading_screen'

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 import 'package:tempo_template/services/location.dart';
 
