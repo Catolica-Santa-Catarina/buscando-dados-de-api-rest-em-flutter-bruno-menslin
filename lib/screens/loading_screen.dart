b6115a857b8c879254f256b06a0cdfcb76ef33a4
bruno-menslin <brunomenslin@outlook.com>
2022-11-02 12:20:09 -0300

    modify 'getData' in 'loading_screen' to use 'NetworkHelper' class

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:tempo_template/services/location.dart';
+import 'package:tempo_template/services/networking.dart';
 
 const apiKey = '3139fffd8993df60af114840d40d010a';
 
@@ -17,22 +18,11 @@ class _LoadingScreenState extends State<LoadingScreen> {
   late double longitude;
 
   void getData() async {
-    var url = Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey&units=metric');
-    http.Response response = await http.get(url);
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/'
+        'data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey&units=metric');
 
-    if (response.statusCode == 200) {
-      // se a requisição foi feita com sucesso
-      var data = response.body;
-      var jsonData = jsonDecode(data);
-      var cityName = jsonData['name'];
-      var temperature = jsonData['main']['temp'];
-      var weatherCondition = jsonData['weather'][0]['id'];
-      print(
-          'cidade: $cityName, temperatura: $temperature, condição: $weatherCondition');
-    } else {
-      print(response.statusCode); // senão, imprima o código de erro
-    }
+    var weatherData = await networkHelper.getData();
   }
 
   void getLocation() async {
