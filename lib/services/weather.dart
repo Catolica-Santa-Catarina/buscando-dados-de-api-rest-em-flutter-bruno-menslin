5ba7396171010003aecea13c0d4d2e0aec9a717a
bruno-menslin <brunomenslin@outlook.com>
2022-11-02 14:56:01 -0300

    create 'getLocationWeather' method in 'weather' service

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,22 @@
+import 'package:tempo_template/services/location.dart';
+import 'package:tempo_template/services/networking.dart';
+
+const apiKey = '3139fffd8993df60af114840d40d010a';
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}'
+        '&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
